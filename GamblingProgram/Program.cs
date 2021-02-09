95f45e4655f14eba94750914abc1d5fb7f8fd733
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-09 15:20:34 +0530

    [vijay]IniilizedVariables

---
+++ GamblingProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GamblingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamblerDetails();
+        }
+        public static void GamblerDetails()
+        {
+            int gamblerAmountDay = 100;
+            int betEachGame = 1;
+
+        }
+    }
+}
