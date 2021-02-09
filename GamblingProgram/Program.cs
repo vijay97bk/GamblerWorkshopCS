23b70d3b804e63d21f3afa154de96eaf58f48b93
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-09 16:10:19 +0530

    [vijay]Added20DaysValue

--- GamblingProgram/Program.cs
+++ GamblingProgram/Program.cs
@@ -7,30 +7,42 @@ namespace GamblingProgram
        public static int gamblerAmountDay = 100;
         public static int gamblerHalfAmount = 50;
         public static int betEachGame = 1;
+        public static int totalDays = 20;
         static void Main(string[] args)
         {
-            
                 GamblerBet();
            
         }
      
         public static void GamblerBet()
         {
-            while (gamblerAmountDay <= gamblerHalfAmount)
+            int amountOfTwentyDays = 0;
+            for (int i = 0; i <= totalDays; i++)
             {
-                Random random = new Random();
-                int bet = random.Next(0, 2);
-                if (bet == 0)
+                while (gamblerAmountDay <= gamblerHalfAmount)
                 {
-                    Console.WriteLine("lost the bet");
-                    gamblerAmountDay--;
+                    Random random = new Random();
+                    int bet = random.Next(0, 2);
+                    if (bet == 0)
+                    {
+                        Console.WriteLine("lost the bet");
+                        gamblerAmountDay--;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("you won the bet");
+                        gamblerAmountDay++;
+                    }
 
                 }
+                amountOfTwentyDays += gamblerAmountDay;
+                if (gamblerAmountDay < 100)
+                    Console.WriteLine("you lost " + (100 - gamblerAmountDay) + "amount in 20 days");
                 else
-                {
-                    Console.WriteLine("you won the bet");
-                    gamblerAmountDay++;
-                }
+                    Console.WriteLine("you won amount" + (gamblerAmountDay - 100))
+
+               
             }
 
          
